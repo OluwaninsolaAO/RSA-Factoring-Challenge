b379f75cbb500b1c6e64e44f58e2be9342b6b3c7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-25 03:18:14 +0000

    Task 00: Python equivallent

---
+++ factors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""RSA Factoring Challenge"""
+
+import sys
+try:
+    if sys.argv[1]:
+        pass
+except IndexError:
+    print("USAGE: {} <file>".format(sys.argv[0]))
+    sys.exit(-1)
+
+try:
+    with open(sys.argv[1], "r") as fi:
+        contents = fi.readlines()
+        for line in contents:
+            message="{:d}={:d}*{:d}"
+            test_num = 2
+            num = int(line)
+            if num == 1:
+                print(message.format(num, num, num))
+            while num > 0 and test_num != num and (num % test_num) != 0:
+                test_num += 1
+            print(message.format(num, int(num / test_num), test_num))
+except FileNotFoundError:
+    print("E: Unable to open file \"{}\"".format(sys.argv[1]))
+    sys.exit(-1)
